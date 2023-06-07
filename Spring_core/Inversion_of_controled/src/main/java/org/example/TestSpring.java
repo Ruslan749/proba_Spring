4ba0ad297c500918269dace81c73342f42264a65
ruslan749 <73393054+Ruslan749@users.noreply.github.com>
2023-06-07 23:16:39 +0500

    app_2

---
+++ Spring_core/Inversion_of_controled/src/main/java/org/example/TestSpring.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpring {
+    public static void main(String[] args) {
+        // ClassPathXmlApplicationContext -- возьмет файл конфигурации
+        // прочтет и примет к проекту
+
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+                "applicationContext.xml"
+        );
+
+        // вызовет экземпляр объекта и применин настройки конфигурации
+        // ( id  конфигурации, экземпляр класа на что применить изменения)
+       Music music = context.getBean("musicBean", Music.class);
+
+       MusicPlayer musicPlayer = new MusicPlayer(music);
+        // вызов экземпляра объекта
+       musicPlayer.playMusic();
+        // закрытия контекста
+       context.close();
+    }
+}
